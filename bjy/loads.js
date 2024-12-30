aac84678a3977630a1df5270098e8b5cd860f1ae
hiroshimada <lajunastudio@gmail.com>
2024-12-31 02:39:34 +0700

    Create loads.js

---
+++ bjy/loads.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : 'b7de6d42176768af896d8cf77e8f7bb7',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/b7de6d42176768af896d8cf77e8f7bb7/invoke.js"></scr' + 'ipt>');
