f2c38ecb0560f706f2657ced6c186a57204cc2ec
hiroshimada <lajunastudio@gmail.com>
2024-12-31 13:35:06 +0700

    Create by.js

---
+++ cleo/by.js
@@ -0,0 +1,11 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+// ads-here
+
+document.write('<script type="text/javascript" src="https://coyagami.github.io/x/loading.js"><\/script>');
+
+// --
+
+document.write('</div>');
+document.write('</div>');
