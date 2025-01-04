4039f81a3a0e1aec098a9bb9a390f9dd07ab8832
hiroshimada <lajunastudio@gmail.com>
2025-01-04 12:04:18 +0700

    Create float.js

---
+++ x/float.js
@@ -0,0 +1,18 @@
+document.write('<div id="floatads" style="width: 100%;margin: auto;text-align: center;position: fixed;bottom: -270px;left: 50%;transform: translateX(-50%);z-index: 9999;overflow: hidden;">');
+document.write('<div style="text-align:center;display:block;max-width:750px;height:300px;overflow:hidden;margin:auto">');
+
+
+//ads-start
+	atOptions = {
+		'key' : '1e8cfb88a9b5827f12642a9098d2b752',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/1e8cfb88a9b5827f12642a9098d2b752/invoke.js"></scr' + 'ipt>');
+
+//ads-end
+
+document.write('</div>');
+document.write('</div>');
