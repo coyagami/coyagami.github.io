8cac65dc5b422f23f0b6ff1c36a3778c4927c97f
hiroshimada <lajunastudio@gmail.com>
2025-05-20 16:55:05 +0700

    Create float.js

---
+++ watchnow/float.js
@@ -0,0 +1,11 @@
+document.write('<div id="floatads" style="width: 100%;margin: auto;text-align: center;position: fixed;bottom: -270px;left: 50%;transform: translateX(-50%);z-index: 9999;overflow: hidden;">');
+document.write('<div style="text-align:center;display:block;max-width:750px;height:300px;overflow:hidden;margin:auto">');
+
+
+// ads-start
+
+
+// ads-end
+
+document.write('</div>');
+document.write('</div>');
